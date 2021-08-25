7f66e3f57ab75800513a0bdf7dddbaae4eb9faa7
Jóhann Østerø <josst@ft.fo>
2021-08-25 20:31:35 +0100

    added a quick precaution check to make the song trigger's collider automatically into a trigger (if possible)

--- Assets/Scripts/Audio/Music/SongTrigger.cs
+++ Assets/Scripts/Audio/Music/SongTrigger.cs
@@ -5,6 +5,15 @@ public class SongTrigger : MonoBehaviour
     public MusicManager manager;
     public Song song;
 
+    private void Start()
+    {
+        var collider = GetComponent<Collider2D>();
+        if (collider)
+        {
+            collider.isTrigger = true;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         manager.PlaySong(song);
